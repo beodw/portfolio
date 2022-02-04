90798d717a43642967d532dc50b3747c5db8c032
beodwilson <beodwilson@gmail.com>
2022-02-04 00:11:00 +0000

    Updates

---
+++ precache-manifest.55cf77f4d900464880f544532bc1d8ac.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5ac5f33cf8a6a51a0cff7243803303ba",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "8e3546c4de8100efa7cf",
+    "url": "/portfolio/static/css/2.d64471e2.chunk.css"
+  },
+  {
+    "revision": "005c8e0c27eca39a91cf",
+    "url": "/portfolio/static/css/main.db6f0745.chunk.css"
+  },
+  {
+    "revision": "8e3546c4de8100efa7cf",
+    "url": "/portfolio/static/js/2.4efe58fd.chunk.js"
+  },
+  {
+    "revision": "4386012a6d334684338618496b7aac20",
+    "url": "/portfolio/static/js/2.4efe58fd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "005c8e0c27eca39a91cf",
+    "url": "/portfolio/static/js/main.da901616.chunk.js"
+  },
+  {
+    "revision": "6c972d9418aa5b4e59ab",
+    "url": "/portfolio/static/js/runtime-main.1b893ad1.js"
+  }
+]);
